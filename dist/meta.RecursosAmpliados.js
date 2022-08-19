9dfb004f5d59dc1b2cc0388435249f8fecc0553f
Ivan Ramírez <ivan_amigue@hotmail.com>
2022-08-19 12:11:29 -0500

    Versión adaptada a OGame v8 con mejoras considerables en CSS

---
+++ dist/meta.RecursosAmpliados.js
@@ -0,0 +1,18 @@
+﻿// ==UserScript==
+// @name OGame: Recursos Ampliados
+// @description OGame: Detalla la produccion de recursos en Opciones de Recursos
+// @version 2.9
+// @creator jgarrone
+// @copyright 2016, jgarrone, Actualización por BigBoss (JBWKZ2099)
+// @homepageURL https://openuserjs.org/scripts/jgarrone/OGame_Recursos_Ampliados
+// @supportURL https://openuserjs.org/scripts/jgarrone/OGame_Recursos_Ampliados/issues
+// @updateURL https://openuserjs.org/meta/jgarrone/OGame_Recursos_Ampliados.meta.js
+// @downloadURL https://openuserjs.org/src/scripts/jgarrone/OGame_Recursos_Ampliados.user.js
+// @match *://*/game/index.php?page=ingame&component=resourcesettings*
+// @match *://*/game/index.php?page=ingame&component=resourceSettings*
+// @grant none
+// @license MIT
+// Se le hicieron solo unas modificaciones para que trabaje en el version de Ogame 6.5.1
+// @author jgarrone todos los creditos // Aporte de labelladurmiente para actualizar a Versión 7.1
+//
+// ==/UserScript==
