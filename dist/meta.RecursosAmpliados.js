0960e4b258244526f17fa0a8e168ea75a613178d
Ivan Ramírez <ivan_amigue@hotmail.com>
2022-11-25 17:46:30 -0600

    Header updates

--- dist/meta.RecursosAmpliados.js
+++ dist/meta.RecursosAmpliados.js
@@ -1,13 +1,13 @@
 ﻿// ==UserScript==
 // @name OGame: Recursos Ampliados
 // @description OGame: Detalla la produccion de recursos en Opciones de Recursos
-// @version 2.991
+// @version 3
 // @creator jgarrone
 // @copyright 2016, jgarrone, Actualización por BigBoss (JBWKZ2099)
-// @homepageURL https://openuserjs.org/scripts/jgarrone/OGame_Recursos_Ampliados
-// @supportURL https://openuserjs.org/scripts/jgarrone/OGame_Recursos_Ampliados/issues
-// @updateURL https://openuserjs.org/meta/jgarrone/OGame_Recursos_Ampliados.meta.js
-// @downloadURL https://openuserjs.org/src/scripts/jgarrone/OGame_Recursos_Ampliados.user.js
+// @homepageURL https://github.com/JBWKZ2099/ogame-recursos-ampliados
+// @supportURL https://github.com/JBWKZ2099/ogame-recursos-ampliados/issues
+// @updateURL https://raw.githubusercontent.com/JBWKZ2099/ogame-recursos-ampliados/master/dist/user.RecursosAmpliados.js
+// @downloadURL https://raw.githubusercontent.com/JBWKZ2099/ogame-recursos-ampliados/master/dist/user.RecursosAmpliados.js
 // @match *://*/game/index.php?page=ingame&component=resourcesettings*
 // @match *://*/game/index.php?page=ingame&component=resourceSettings*
 // @match *://*/game/index.php?page=ingame&component=research*
